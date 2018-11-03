71fedada87fc11fc756c257ec3de4abb55b3c6f1
shattenjager <pavan_thomas@yahoo.com>
2018-11-02 20:49:13 -0700

    First commit

---
+++ src/com/java/rest/RestClient.java
@@ -0,0 +1,43 @@
+package com.java.rest;
+
+
+
+public class RestClient {
+	public static void main (String args[]) {
+		public getLargestCityAndCapital(String state) throws Exception {
+
+	        RestTemplate restTemplate = new RestTemplate();
+	        JsonObject apiResponse = new JsonParser().parse(
+	                restTemplate.getForEntity("http://services.groupkt.com/state/get/USA/all", String.class).getBody()).getAsJsonObject();
+	        JsonArray result = apiResponse.get("RestResponse").getAsJsonObject().get("result").getAsJsonArray();
+
+	        for (int i = 0; i < result.size(); i++) {
+
+	            JsonObject entry = result.get(i).getAsJsonObject();
+	            if (entry.get("name").getAsString().equalsIgnoreCase(state.trim())
+	                    || entry.get("abbr").getAsString().equalsIgnoreCase(state.trim())) {
+
+	                if (entry.has("largest_city")) {
+	                    System.out.println("Largest City => " + entry.get("largest_city").getAsString());
+	                } else {
+	                    System.out.println("Sorry, the largest City did not exist in our system.");
+	                }
+
+	                if (entry.has("capital")) {
+	                    System.out.println("Capital => " + entry.get("capital").getAsString());
+	                } else {
+	                    System.out.println("Sorry, the capital did not exist in our system.");
+	                }
+
+	                break;
+	            }
+
+	        }
+
+	        System.out.println(
+	                "Sorry, the state '" + state + "' was invalid.  Please enter a valid state name or abbreviation:");
+
+	    }
+	}
+
+}
