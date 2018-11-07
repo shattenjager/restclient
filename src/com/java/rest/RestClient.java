d27d5cf606ed2d135862048f02c9ba15eaaeee82
Thomas <pavan_thomas@yahoo.com>
2018-11-06 18:11:21 -0800

    Add files via upload
    New commit

--- src/com/java/rest/RestClient.java
+++ src/com/java/rest/RestClient.java
@@ -1,43 +1,57 @@
 package com.java.rest;
 
-
+import java.io.*;
+import java.net.*;
+import javax.xml.xpath.*;
+import org.apache.http.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.*;
+import org.apache.http.entity.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.util.*;
+import org.xml.sax.*;
 
 public class RestClient {
-	public static void main (String args[]) {
-		public getLargestCityAndCapital(String state) throws Exception {
+	public static void main(String args[]) {
 
-	        RestTemplate restTemplate = new RestTemplate();
-	        JsonObject apiResponse = new JsonParser().parse(
-	                restTemplate.getForEntity("http://services.groupkt.com/state/get/USA/all", String.class).getBody()).getAsJsonObject();
-	        JsonArray result = apiResponse.get("RestResponse").getAsJsonObject().get("result").getAsJsonArray();
+	}
 
-	        for (int i = 0; i < result.size(); i++) {
+	public static void getLargestCityAndCapital(String state) throws Exception {
 
-	            JsonObject entry = result.get(i).getAsJsonObject();
-	            if (entry.get("name").getAsString().equalsIgnoreCase(state.trim())
-	                    || entry.get("abbr").getAsString().equalsIgnoreCase(state.trim())) {
+		//RestTemplate restTemplate = new RestTemplate();
+		rest
+		JsonObject apiResponse = new JsonParser().parse(
+				restTemplate.getForEntity("http://services.groupkt.com/state/get/USA/all", String.class).getBody())
+				.getAsJsonObject();
+		JsonArray result = apiResponse.get("RestResponse").getAsJsonObject().get("result").getAsJsonArray();
 
-	                if (entry.has("largest_city")) {
-	                    System.out.println("Largest City => " + entry.get("largest_city").getAsString());
-	                } else {
-	                    System.out.println("Sorry, the largest City did not exist in our system.");
-	                }
+		for (int i = 0; i < result.size(); i++) {
 
-	                if (entry.has("capital")) {
-	                    System.out.println("Capital => " + entry.get("capital").getAsString());
-	                } else {
-	                    System.out.println("Sorry, the capital did not exist in our system.");
-	                }
+			JsonObject entry = result.get(i).getAsJsonObject();
+			if (entry.get("name").getAsString().equalsIgnoreCase(state.trim())
+					|| entry.get("abbr").getAsString().equalsIgnoreCase(state.trim())) {
 
-	                break;
-	            }
+				if (entry.has("largest_city")) {
+					System.out.println("Largest City => " + entry.get("largest_city").getAsString());
+				} else {
+					System.out.println("Sorry, the largest City did not exist in our system.");
+				}
 
-	        }
+				if (entry.has("capital")) {
+					System.out.println("Capital => " + entry.get("capital").getAsString());
+				} else {
+					System.out.println("Sorry, the capital did not exist in our system.");
+				}
 
-	        System.out.println(
-	                "Sorry, the state '" + state + "' was invalid.  Please enter a valid state name or abbreviation:");
+				break;
+			}
 
-	    }
+		}
+
+		System.out.println(
+				"Sorry, the state '" + state + "' was invalid.  Please enter a valid state name or abbreviation:");
+
 	}
 
 }
